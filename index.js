a1531ae5a5326e64073fc7c4bff827c5fd6e497f
Your Name01~ <you@example.com>
2023-04-28 21:13:29 -0300

    exercicio

---
+++ index.js
@@ -0,0 +1,72 @@
+// // // const nome = prompt("Nome?:")
+// // // console.log(`o "${nome}" é turista`);
+
+// // // const nome = "maria"
+// // // const idade = 15
+
+// // // console.log("seu nome"+nome+"idade"+idade)
+
+// // // const nome = prompt("Nome")
+// // // const cor = prompt("cor favorita ")
+// // // console.log("A cor favorita",nome ,"é", cor)
+
+// // const nome = prompt("Digite seu nome")
+// // nome.lenght === 0 ? 
+// // console.log ("Voce precisa digitr algo"):
+// // console.log("o nome é",nome);
+
+// const sobrenome = "ZUkenberg"
+// console.log(sobrenome.toLowerCase())
+// console.log(sobrenome.toUpperCase())
+
+// const nome = "    maria"
+// console.log(nome.trim());
+
+// const receita = "bolo de CENOURA".toLowerCase()
+// const novaReceita = receita.replaice("cenoura","abacaxi")
+// console.log(novaReceita);
+// // console.log(frase.trim().toLowerCase().includes(cenoura))
+
+
+// const nome = "diennyfer"
+// console.log(nome.toUpperCase())
+
+// const receita = "bolo de chocolate branco"
+// const novaReceita = receita.replaceAll("o","i")
+// console.log(novaReceita.length);
+
+// const numeros = [14, 242, 673, 534, 5325];
+// console.log(numeros[0]);
+
+// const cachorro = ["caramelo","buldogue","labrador","golden","yorkshire"];
+// cachorro.splice(1,1)
+
+// console.log(cachorro)
+
+// cachorro.pop()
+// console.log(cachorro)
+
+// onsole.log(cachorro.length);
+// console.log(cachorro.includes("caramelo"));
+// cachorro.push("Outro cachorro qualquer")
+// console.log(cachorro);c
+
+// const racaCachorro = prompt("raça de cachorro")
+// cachorro.push(racaCachorro)
+// console.log(cachorro);
+
+const numeros = [1, 2, 3, 4, 5, 6,];
+console.log(numeros.length);
+console.log(numeros);
+numeros.push(7)
+numeros.splice(3,2);
+console.log(numeros);
+console.log(numeros.length);
+
+
+
+
+
+// const numero = Number(prompt("Digite um numero de 0 a 4"))
+// console.log(cachorro[numero]);
+
